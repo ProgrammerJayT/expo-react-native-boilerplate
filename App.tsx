4f2818751ad0ab0f4ac399b8fcd09fe0e634901d
ProgrammerJayT <theanthem8@gmail.com>
2024-06-28 00:52:17 +0200

    Wrap entry file and feedback components container with feedback components state provider

--- App.tsx
+++ App.tsx
@@ -1,23 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import SpinnerComponent from "./src/layout/spinner";
 import React from "react";
+import RouteStack from "./src/route";
+import { FeedbackComponentsStateProvider } from "./src/context/feedback";
+import FeedbackComponentsContainer from "./src/layout/feedback";
 
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <SpinnerComponent />
-    </View>
+    <FeedbackComponentsStateProvider>
+      <FeedbackComponentsContainer />
+      <RouteStack />
+    </FeedbackComponentsStateProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
 
 export default App;
