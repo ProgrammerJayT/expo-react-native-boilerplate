94d1ff951dd63cebb32c3638cfdd18dd2eec553b
ProgrammerJayT <theanthem8@gmail.com>
2024-06-28 00:58:45 +0200

    Create feedback components layout styles

---
+++ src/layout/feedback/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    position: "absolute",
+    zIndex: 9999,
+    width: "100%",
+    height: "100%",
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
