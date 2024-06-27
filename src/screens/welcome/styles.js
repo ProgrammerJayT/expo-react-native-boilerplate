32a972b74d228ea1f3d5e6b84728dec1d88921d7
ProgrammerJayT <theanthem8@gmail.com>
2024-06-28 00:50:48 +0200

    Create welcome screen styles

---
+++ src/screens/welcome/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
