e7cacfaaa4b8a234eccce22312b75cd6bf7720ab
ProgrammerJayT <theanthem8@gmail.com>
2024-06-28 00:59:10 +0200

    Add welcome screen

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/home";
+import WelcomeScreen from "../screens/welcome";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ const RouteStack: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
